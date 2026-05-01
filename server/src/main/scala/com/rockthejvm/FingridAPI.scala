9e8bee4b6b15dfa54309bdee52cda925e8dc83a8
rosegoldnini <natureninas@gmail.com>
2026-05-01 22:30:08 +0300

    Added fetching data from the API and reading/writing/saving a file

--- server/src/main/scala/com/rockthejvm/FingridAPI.scala
+++ server/src/main/scala/com/rockthejvm/FingridAPI.scala
@@ -13,13 +13,21 @@ object FingridDatasets:
 
 object FingridClient:
 
-  private val ApiKey = sys.env.getOrElse("FINGRID_API_KEY", "7d6b56142a1c467088c41bdb4e1c7af9")
+  private val ApiKey  = sys.env.getOrElse("FINGRID_API_KEY", "7d6b56142a1c467088c41bdb4e1c7af9")
   private val BaseUrl = "https://data.fingrid.fi/api"
   private val Fmt     = DateTimeFormatter.ISO_OFFSET_DATE_TIME
 
   def fetch(datasetId: Int, source: String,
             startTime: ZonedDateTime, endTime: ZonedDateTime): Either[String, List[EnergyReading]] =
     fetchAllPages(datasetId, source, startTime, endTime)
+
+  def save(readings: List[EnergyReading], path: String): Unit =
+    val file = new java.io.FileWriter(path, true)
+    try
+      readings.foreach: r =>
+        file.write(s"${r.source},${r.startTime},${r.endTime},${r.valueMW}\n")
+    finally
+      file.close()
 
   @annotation.tailrec
   private def fetchAllPages(datasetId: Int, source: String,
@@ -44,3 +52,30 @@ object FingridClient:
         if page >= lastPage then Right(acc ++ readings)
         else fetchAllPages(datasetId, source, start, end, page + 1, acc ++ readings)
     catch case e: Exception => Left(e.getMessage)
+
+object FileIO:
+  def write(readings: List[EnergyReading], path: String): Unit =
+    val file = new java.io.FileWriter(path, true)
+    try
+      readings.foreach: r =>
+        file.write(s"${r.source},${r.startTime},${r.endTime},${r.valueMW}\n")
+    finally
+      file.close()
+
+  def read(path: String): List[EnergyReading] =
+    val file = new java.io.BufferedReader(new java.io.FileReader(path))
+    try
+      Iterator.continually(file.readLine())
+        .takeWhile(_ != null)
+        .flatMap: line =>
+          line.split(",") match
+            case Array(src, start, end, value) =>
+              try Some(EnergyReading(src,
+                ZonedDateTime.parse(start),
+                ZonedDateTime.parse(end),
+                value.toDouble))
+              catch case _: Exception => None
+            case _ => None
+        .toList
+    finally
+      file.close()
