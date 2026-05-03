d10ac8665870454ee288bb86af15e7e31d8e1273
davidfrmn <furmandavid@proton.me>
2026-05-03 17:51:52 +0300

    production status based on consumption

--- server/src/main/scala/com/rockthejvm/DataStatusUtils.scala
+++ server/src/main/scala/com/rockthejvm/DataStatusUtils.scala
@@ -2,58 +2,57 @@ package com.rockthejvm
 
 case class DataStatus(
     severity: String,
-    message: String,
-    data: List[DataRow]
+    message: String
 )
 
 
 object DataStatusUtils {
-    def getStatus(data: List[DataRow], threshold: Double): DataStatus = {
+    def getStatus(data: List[DataRow]): DataStatus = {
         val malfunctions = getMalfunctions(data)
-        val lowProduction = getLowProduction(data, threshold)
-        val okay = getOkay(data, threshold)
+        
+        val totalProduced  = getTotalProduction(data)
+        val totalConsumed  = getTotalConsumption(data)
+        val isLowProduction = totalProduced < totalConsumed
 
-        if (malfunctions.nonEmpty && lowProduction.nonEmpty){
-            val message = s"Malfunctions and low production detected"
-            DataStatus("Red", message, malfunctions ++ lowProduction)
-        
-        } else if (malfunctions.nonEmpty) {
-            val message = s"Malfunctions detected"
-            DataStatus("Red", message, malfunctions)
-        
-        } else if (lowProduction.nonEmpty){
-            val message = s"Low Production: ${lowProduction.length}"
-            DataStatus("Yellow", message, lowProduction)
-        
-        } else {
-            val message = "Nothing requires attention"
-            DataStatus("Green", message, okay)
-        }
+        if (malfunctions.nonEmpty && isLowProduction)
+            DataStatus("Red", "Malfunctions and low production detected")
+        else if (malfunctions.nonEmpty)
+            DataStatus("Red", "Malfunctions detected")
+        else if (isLowProduction)
+            DataStatus("Yellow", "Low production")
+        else
+            DataStatus("Green", "Nothing requires attention")
     }
 
     def getMalfunctions(data: List[DataRow]) : List[DataRow] = {
         data.filter(datarow => datarow.energyProduction == 0.0)
     }
 
-    def getLowProduction(data: List[DataRow], threshold: Double) : List[DataRow] = {
-        data.filter(datarow => datarow.energyProduction > 0.0 && datarow.energyProduction < threshold)
+    def getTotalProduction(data: List[DataRow]) : Double = {
+        val analytics = AnalyticsHelper.runAnalytics(data)
+        val consumed = getTotalConsumption(data)
+        val sums = analytics.values.map(x => x("sum").asInstanceOf[Double])
+        val production = sums.sum - consumed
+        production
     }
 
-    def getOkay(data: List[DataRow], threshold: Double) : List[DataRow] = {
-        data.filter(datarow => datarow.energyProduction >= threshold)
+    def getTotalConsumption(data: List[DataRow]) : Double = {
+        val consumed = AnalyticsHelper.runAnalytics(data)
+        consumed.get("Consumption") match {
+            case Some(statistics) => statistics("sum").asInstanceOf[Double]
+            case None => 0.0
+        }
     }
 
-    def getMalfunctonsAndLowProduction(data: List[DataRow], threshold: Double) : List[DataRow] = {
-        data.filter(datarow => datarow.energyProduction < threshold)
+    def isProductionLow(data: List[DataRow]) : Boolean = {
+        getTotalConsumption(data)>getTotalProduction(data)
     }
 
     def main(args: Array[String]): Unit = {
         val threshold = 2000
         DataCollectorUtils.fromFile("energy.csv") match {
             case Right(data) =>
-                println(DataStatusUtils.getStatus(data, threshold))
-                println("\n\nUnderperforming")
-                println(getMalfunctonsAndLowProduction(data, threshold))
+                println(getStatus(data))
             case Left(error) =>
                 println(s"Failed to read file: $error")
         }
