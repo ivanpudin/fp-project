c8578eebfd6400977c3d2aaee70ec970340d1d8e
davidfrmn <furmandavid@proton.me>
2026-05-03 05:39:36 +0300

    added error handling to data collector and updated the helper example

--- server/src/main/scala/com/rockthejvm/DataCollectorUtils.scala
+++ server/src/main/scala/com/rockthejvm/DataCollectorUtils.scala
@@ -10,22 +10,60 @@ case class DataRow(
 )
 
 object DataCollectorUtils {
-    def fromFile(filename: String) : List[DataRow] = {
-        def lineToDataRow(line: String) : DataRow = {
+    def Try[A](a: => A): Either[Exception, A] =
+        try Right(a)
+        catch { case e: Exception => Left(e) }
+
+    def fromFile(filename: String) : Either[String, List[DataRow]] = {
+        def lineToDataRow(line: String) : Either[String, DataRow] = {
             val values = line.split(",").map(_.trim)
-            DataRow(
-                values(0),
-                values(1),
-                values(2),
-                values(3).toDouble
-            )
+            
+            if (values.length != 4) {
+                Left(s"Invalid row format (expected columns:4): $line")
+            } else {
+            
+                val production = Try(values(3).toDouble)
+
+                production match {
+                    case Right(energyProduction) =>
+                        Right(DataRow(
+                            values(0),
+                            values(1),
+                            values(2),
+                            energyProduction
+                        ))
+                    case Left(_) => Left(s"Production could not have been parsed to double in line: $line")
+                }
+            }
+            
         }
 
 
-        val source = Source.fromFile(filename)
-        val data = source.getLines().map(line => lineToDataRow(line)).toList
-        source.close()
-        return data
+        Try(Source.fromFile(filename)) match {
+            case Left(e) => Left(s"Could not open $filename. Error: $e")
+            case Right(source) => try {
+                val data = source.getLines().map(line => lineToDataRow(line)).toList
+                
+                val lefts = data.filter(_.isLeft)
+                
+                if (lefts.isEmpty) {
+                    Right(
+                        data.map {
+                            case Right(row) => row
+                            case Left(_) => throw new RuntimeException("Something went wrong")
+                        }
+                    )
+                } else {
+                    lefts.head match {
+                        case Left(errorMessage) => Left(errorMessage)
+                        case _ => Left("Unknown error")
+                    }
+                }
+
+            } finally {
+                source.close()
+            }
+        }
     }
 
     def main(args: Array[String]): Unit = {
